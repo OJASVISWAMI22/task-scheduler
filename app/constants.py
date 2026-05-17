8ea3083cbc2471459209a65b1546fc7ad2b5be70
OJASVISWAMI222 <ojasviswami22@gmail.com>
2026-05-17 14:14:07 +0530

    Set constants along with dotenv file

---
+++ app/constants.py
@@ -0,0 +1,36 @@
+# constants.py
+# Constants across app and services are defined here
+from enum import Enum
+
+# Rate limiter
+RATE_LIMIT_MAX_REQUESTS = 10
+RATE_LIMIT_WINDOW_SECONDS = 20
+RATE_LIMIT_KEY_PREFIX = "rate_limit:"
+
+# Redis queue
+QUEUE_HIGH = "queue:high"
+QUEUE_NORMAL = "queue:normal"
+QUEUE_LOW = "queue:low"
+
+# operation to be performed
+class Operation(str,Enum):
+  ENCRYPT = "Encrypt"
+  HASH = "Hash"
+  TRANSFORM = "Transform"
+
+# Task status
+class TaskStatus(str,Enum):
+  PENDING = "pending"
+  DONE = "done"
+  IN_PROGRESS = "in_progress"
+  FAILED = "failed"
+
+# Task Priority 
+class PriorityLevel(int,Enum):
+  LOW = 1
+  MEDIUM = 2
+  HIGH = 3
+
+# Cpp service url
+
+CPP_SERVICE_URL = "http://localhost:8080"
